eca45677a2d58c3083ed5956bcc514d5b28179d1
shubham14p3 <shubham14p3@gmail.com>
2020-04-19 04:22:47 +0530

    fileSize added

---
+++ lib/filesize.rb
@@ -0,0 +1,15 @@
+class FileSize < Rule
+	def parse(file_data, file_name)
+	  @line = 0
+	  data = file_data.map(&:chomp)
+	  lines = data.count
+	  if lines > 120
+		@broken = true
+		add_to_report(file_name, @line, @name, lines)
+	  else
+		@broken = false
+	  end
+	  @broken
+	end
+  end
+  
