c6d9688fd1de52521f1fb2733d9bf73d2a2e13b6
shubham14p3 <shubham14p3@gmail.com>
2020-04-19 04:19:04 +0530

    Whitespace added

---
+++ lib/whitespaces.rb
@@ -0,0 +1,17 @@
+class WhiteSpace < Rule
+	def parse(file_data, file_name)
+	  data = file_data.map(&:chomp)
+	  @line = 0
+	  @broken = false
+	  data.each do |code_line|
+		@line += 1
+		chars = code_line.split(//)
+		if chars.last == ' '
+		  @broken = true
+		  add_to_report(file_name, @line, @name, nil)
+		end
+	  end
+	  @broken
+	end
+  end
+  
