42e6ea115f79768e89df027374eb069e4a42749c
Shubham Raj <shubham14p3@gmail.com>
2020-04-19 07:35:07 +0530

    Revert "Development"

--- spec/index_code_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/index_code.rb'
-
-describe IndexCode do
-  describe '#initialize' do
-    it 'takes 1 argument as a path of the file' do
-      expect { IndexCode.new('lib/student_codes/student_code1.rb') }.not_to raise_error
-    end
-  end
-end
--- spec/read_file_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/read_file.rb'
-
-describe ReadFile do
-  describe '#initialize' do
-    it 'takes 1 argument as a path of the file' do
-      expect { ReadFile.new('lib/student_codes/student_code1.rb') }.not_to raise_error
-    end
-  end
-end
